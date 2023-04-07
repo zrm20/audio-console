1a1817bf00bf43ab4f5c7255f460dadf2c8ce50c
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-04-06 19:44:31 -0500

    add FT templates

---
+++ .fttemplates/Component/[FTName].test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+
+import <FTName | capitalize> from './[FTName]';
+
+describe('<[FTName] />', () => {
+  it("should render", () => {
+    render(<<FTName> />);
+  });
+
+});
---
+++ .fttemplates/Component/[FTName].tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import useStyles from './[FTName].styles';
+
+interface <FTName | capitalize>Props {
+
+};
+
+export default function <FTName | capitalize>(props: <FTName | capitalize>Props): JSX.Element {
+  const styles = useStyles();
+
+  return (
+    <div>
+
+    </div>
+  );
+};
