7f048d2c7ed4bcfbdacdeff1a9ca1cfe97f43021
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-04-12 10:36:05 -0500

    add group outputs to channel strip

--- src/components/channel/ChannelStrip/ChannelStrip.tsx
+++ src/components/channel/ChannelStrip/ChannelStrip.tsx
@@ -15,12 +15,16 @@ import channelReducer from './channelReducer';
 import { KnobChangeEvent } from 'primereact/knob';
 import { ToggleButtonChangeEvent } from 'primereact/togglebutton';
 import { SliderChangeEvent } from 'primereact/slider';
+import { MIN_DBFS_VALUE } from '../../../constants/audioLevels';
+import { constrainValue } from '../../../utils';
+import GroupOutAssignment from '../GroupOutAssignment/GroupOutAssignment';
 // import { getBusOutput } from '../../../utils';
 
 interface ChannelStripProps {
   name: string;
   inputValue: number;
   auxes: ConsoleBus[];
+  groups: ConsoleBus[];
   size?: number;
 };
 
@@ -34,20 +38,42 @@ function createDefaultAux(auxBus: ConsoleBus): AuxSend {
 };
 
 export default function ChannelStrip(props: ChannelStripProps): JSX.Element {
-  const { name, inputValue, size = 75, auxes } = props;
+  const { name, inputValue, size = 75, auxes, groups } = props;
   const styles = useStyles(size);
   const initialState: ChannelState = {
     isPadded: false,
     preAmpGain: PRE_AMP_MIN_GAIN,
     isMuted: false,
     faderLevel: 0,
-    auxSends: auxes.map(auxBus => createDefaultAux(auxBus))
+    auxSends: auxes.map(auxBus => createDefaultAux(auxBus)),
+    groupOuts: []
   };
   const [state, dispatch] = useReducer(channelReducer, initialState);
 
   // Level pick points
   const padOutput = inputValue + (state.isPadded ? DEFAULT_PAD_LEVEL : 0);
   const preAmpOutput = padOutput + state.preAmpGain;
+  const faderOutput = state.isMuted ? MIN_DBFS_VALUE : preAmpOutput + state.faderLevel;
+  // const busOutputs: ChannelGroupOutput;
+
+  const auxOutputs: ChannelAuxOutput[] = state.auxSends.map(aux => {
+    const auxOut: ChannelAuxOutput = { id: aux.id, value: MIN_DBFS_VALUE };
+
+    if(state.isMuted || aux.isMuted || aux.sendLevel === MIN_DBFS_VALUE) {
+      return auxOut;
+    }
+
+    if(aux.isPreFader) {
+      auxOut.value = preAmpOutput + aux.sendLevel;
+    } else {
+      auxOut.value = faderOutput + aux.sendLevel;
+    };
+
+    auxOut.value = constrainValue(auxOut.value, MIN_DBFS_VALUE, 0);
+
+    return auxOut;
+  });
+
 
   // onChange functions
   function handleGainKnobChange(evt: KnobChangeEvent): void {
@@ -68,6 +94,10 @@ export default function ChannelStrip(props: ChannelStripProps): JSX.Element {
 
   function handleAuxChange(newAuxValues: AuxSend): void {
     dispatch({ type: "SET_AUX", payload: newAuxValues })
+  };
+
+  function handleBusAssignChange(groupId: string): void {
+    dispatch({ type: "TOGGLE_GROUP_ASSIGNMENT", payload: groupId })
   };
  
   return (
@@ -98,6 +128,15 @@ export default function ChannelStrip(props: ChannelStripProps): JSX.Element {
       <MuteButton value={state.isMuted} onChange={handleChangeMute} size={size} />
 
       <Fader value={state.faderLevel} onChange={handleChangeFader} size={size} />
+
+      <Divider />
+
+      <GroupOutAssignment
+        groups={groups}
+        value={state.groupOuts}
+        onChange={handleBusAssignChange}
+        size={size}
+      />
 
       <Divider />
 
