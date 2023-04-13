27c95241ed3bb3dc00e62a9622712322d66f631e
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-04-12 19:35:20 -0500

    move channel state outside of component

--- src/components/channel/ChannelStrip/ChannelStrip.tsx
+++ src/components/channel/ChannelStrip/ChannelStrip.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import { Card, Typography } from '@mui/material';
 import { Divider } from 'primereact/divider';
 
@@ -9,132 +9,62 @@ import BusSend from '../BusSend/BusSend';
 import Fader from '../Fader/Fader';
 import Meter from '../Meter/Meter';
 import MuteButton from '../MuteButton/MuteButton';
-import { ChannelState } from './channelReducer.types';
-import { BUS_MIN_GAIN, DEFAULT_PAD_LEVEL, PRE_AMP_MIN_GAIN } from '../../../constants/busLevels';
-import channelReducer from './channelReducer';
-import { KnobChangeEvent } from 'primereact/knob';
-import { ToggleButtonChangeEvent } from 'primereact/togglebutton';
-import { SliderChangeEvent } from 'primereact/slider';
+import { BUS_MIN_GAIN, DEFAULT_PAD_LEVEL } from '../../../constants/busLevels';
 import { MIN_DBFS_VALUE } from '../../../constants/audioLevels';
-import { constrainValue } from '../../../utils';
 import GroupOutAssignment from '../GroupOutAssignment/GroupOutAssignment';
-// import { getBusOutput } from '../../../utils';
+import { useChannelState } from '../../../hooks';
 
 interface ChannelStripProps {
   name: string;
   inputValue: number;
-  auxes: ConsoleBus[];
-  groups: ConsoleBus[];
+  auxes: ConsoleBusInitializer[];
+  groups: ConsoleBusInitializer[];
+  id: string;
   size?: number;
-};
-
-function createDefaultAux(auxBus: ConsoleBus): AuxSend {
-  return {
-    ...auxBus,
-    isMuted: false,
-    isPreFader: true,
-    sendLevel: BUS_MIN_GAIN
-  }
 };
 
 export default function ChannelStrip(props: ChannelStripProps): JSX.Element {
   const { name, inputValue, size = 75, auxes, groups } = props;
   const styles = useStyles(size);
-  const initialState: ChannelState = {
-    isPadded: false,
-    preAmpGain: PRE_AMP_MIN_GAIN,
-    isMuted: false,
-    faderLevel: 0,
-    auxSends: auxes.map(auxBus => createDefaultAux(auxBus)),
-    groupOuts: []
-  };
-  const [state, dispatch] = useReducer(channelReducer, initialState);
+  const { state, handleChange }  = useChannelState();
 
   // Level pick points
   const padOutput = inputValue + (state.isPadded ? DEFAULT_PAD_LEVEL : 0);
   const preAmpOutput = padOutput + state.preAmpGain;
-  const faderOutput = state.isMuted ? MIN_DBFS_VALUE : preAmpOutput + state.faderLevel;
-  // const busOutputs: ChannelGroupOutput;
+  // const faderOutput = state.isMuted ? MIN_DBFS_VALUE : preAmpOutput + state.faderLevel;
 
-  const auxOutputs: ChannelAuxOutput[] = state.auxSends.map(aux => {
-    const auxOut: ChannelAuxOutput = { id: aux.id, value: MIN_DBFS_VALUE };
-
-    if(state.isMuted || aux.isMuted || aux.sendLevel === MIN_DBFS_VALUE) {
-      return auxOut;
-    }
-
-    if(aux.isPreFader) {
-      auxOut.value = preAmpOutput + aux.sendLevel;
-    } else {
-      auxOut.value = faderOutput + aux.sendLevel;
-    };
-
-    auxOut.value = constrainValue(auxOut.value, MIN_DBFS_VALUE, 0);
-
-    return auxOut;
-  });
-
-
-  // onChange functions
-  function handleGainKnobChange(evt: KnobChangeEvent): void {
-    dispatch({ type: "SET_PRE_AMP_GAIN", payload: evt.value })
-  };
-
-  function handlePadChange(evt: ToggleButtonChangeEvent): void {
-    dispatch({ type: "SET_IS_PADDED", payload: evt.value })
-  };
-
-  function handleChangeMute(evt: ToggleButtonChangeEvent): void {
-    dispatch({ type: "SET_IS_MUTED", payload: evt.value })
-  };
-
-  function handleChangeFader(evt: SliderChangeEvent): void {
-    dispatch({ type: "SET_FADER", payload: evt.value as number })
-  };
-
-  function handleAuxChange(newAuxValues: AuxSend): void {
-    dispatch({ type: "SET_AUX", payload: newAuxValues })
-  };
-
-  function handleBusAssignChange(groupId: string): void {
-    dispatch({ type: "TOGGLE_GROUP_ASSIGNMENT", payload: groupId })
-  };
- 
   return (
     <Card variant='outlined' sx={styles.root}>
       <Meter signalLevel={preAmpOutput} size={size}/>
 
       <Divider />
 
-      <GainKnob value={state.preAmpGain} onChange={handleGainKnobChange} size={size}/>
-      <PadSwitch value={state.isPadded} onChange={handlePadChange} size={size} />
+      <GainKnob value={state.preAmpGain} onChange={handleChange.preAmpGain} size={size}/>
+      <PadSwitch value={state.isPadded} onChange={handleChange.isPadded} size={size} />
 
       <Divider />
       {
-        state.auxSends.map(aux => (
+        auxes.map(aux => (
           <BusSend
             key={aux.id}
             id={aux.id}
             name={aux.name}
-            value={aux.sendLevel}
-            onChange={handleAuxChange}
-            isPreFader={aux.isPreFader}
             size={size}
           />
         ))
       }
       <Divider />
 
-      <MuteButton value={state.isMuted} onChange={handleChangeMute} size={size} />
+      <MuteButton value={state.isMuted} onChange={handleChange.isMuted} size={size} />
 
-      <Fader value={state.faderLevel} onChange={handleChangeFader} size={size} />
+      <Fader value={state.faderLevel} onChange={handleChange.faderLevel} size={size} />
 
       <Divider />
 
       <GroupOutAssignment
         groups={groups}
         value={state.groupOuts}
-        onChange={handleBusAssignChange}
+        onChange={handleChange.groupOuts}
         size={size}
       />
 
